581f7b4587065e0ddd5fd406c49ef73649c45ea5
droff <adroff@gmail.com>
2016-06-03 16:03:16 +0300

    support methods

--- lib/nes.rb
+++ lib/nes.rb
@@ -1,27 +1,10 @@
+require './lib/ext'
 require './lib/nes/asm'
 require './lib/nes/cpu'
 require './lib/nes/memory'
 
+Fixnum.include(Ext)
+NilClass.include(Ext)
+
 module NES
-end
-
-module NESMP
-  def to_hex
-    self ?  (self > 0xff ? "%04X" : "%02X") % self : ''
-  end
-
-  def lo
-    self & 0xff
-  end
-
-  def hi
-    self >> 8
-  end
-end
-
-Fixnum.include(NESMP)
-NilClass.include(NESMP)
-
-def to_dump(a)
-  a.map { |e| e.to_i(16) }
 end
